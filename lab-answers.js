f8e464c20580dbabae8339976ab6a3401b8e4f7e
olgaosadchuk <osadchuk.olga.o@gmail.com>
2023-05-29 20:52:22 -0700

    Wild Wild Life answered

--- lab-answers.js
+++ lab-answers.js
@@ -33,3 +33,23 @@ for (let i = 1; i <= 100; i++) {
   }
 
 
+
+/////////////////////////////////
+//Wild Wild Life
+////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2]++; 
+wolfy[3] = "Gotham City";
+dart.push("Hawkins");
+wolfy.splice(0, 1, "Gameboy");
+
+console.log(wolfy);
+console.log(sharky);
+console.log(plantee);
+console.log(porgee);
+console.log(dart);
